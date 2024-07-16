2c40fbbcac848fe515c2d2aeaa0f567f5a626911
ritik dave <ritik.dave@intimetec.com>
2024-07-16 11:32:17 +0530

    commited

--- week_1_assignment/js_dom/script.js
+++ week_1_assignment/js_dom/script.js
@@ -1,8 +1,81 @@
-function onsubmit(){
-    let x = document.forms["myform"]["fname"].value;
 
-    if(x==""){
-        alert("fname is empty");
-    }
 
+
+let input = document.getElementById("input");
+let viewText = document.getElementById("viewText");
+
+let boxes = document.getElementsByClassName("box");
+
+let red = document.getElementById("red");
+let redValue = document.getElementById("redValue")
+
+let greenValue = document.getElementById("greenValue")
+let blueValue = document.getElementById("blueValue")
+
+
+let green = document.getElementById("green");
+let blue = document.getElementById("blue");
+let size = document.getElementById("size");
+let sizeValue = document.getElementById("sizeValue");
+let weight = document.getElementById("weight");
+let weightValue = document.getElementById("weightValue");
+
+
+let innerBox = document.getElementsByClassName("inner-box")[0];
+
+function updateColor(){
+    let r = red.value;
+    let g = green.value;
+    let b = blue.value;
+    viewText.style.color = `rgb(${r},${g},${b})`;
+
+    redValue.innerText = r;
+    greenValue.innerText = g;
+    blueValue.innerText = b;
 }
+
+
+
+
+
+document.addEventListener("DOMContentLoaded",()=>{
+    input.addEventListener("input",()=>{
+        viewText.innerText  = input.value;
+    })
+
+    Array.from(boxes).forEach(box=>{
+        box.addEventListener("click",()=>{
+            viewText.className = "";
+            viewText.classList.add(box.classList[0]);
+            1
+        })
+        box.addEventListener("mouseover",()=>{
+            box.firstChild.style.fontSize = "3rem"
+           
+        })
+        box.addEventListener("mouseout",()=>{
+            box.firstChild.style.fontSize="";
+            
+        })
+    })
+
+    size.addEventListener("input",()=>{
+        viewText.style.fontSize = `${size.value/10}rem`
+        sizeValue.innerText = size.value;
+    })
+    weight.addEventListener("input",()=>{
+        viewText.style.fontWeight = `${weight.value}`
+        weightValue.innerText = weight.value;
+    })
+
+    red.addEventListener("input",updateColor);
+    green.addEventListener("input",updateColor);
+    blue.addEventListener("input",updateColor);
+
+    
+
+    
+
+
+
+})
